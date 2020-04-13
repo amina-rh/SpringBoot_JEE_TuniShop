d4b2e57bcb948ef47e75b4e3dc63de2830b96679
am_rh <amina.rhaiem.94@gmail.com>
2020-04-13 16:29:31 +0200

    Add Search bar

--- src/main/java/org/projetjee/web/ProduitController.java
+++ src/main/java/org/projetjee/web/ProduitController.java
@@ -34,17 +34,24 @@ public class ProduitController {
 	public String products(Model model, 
 			@RequestParam(name="page", defaultValue = "0")int page,
 			//içi même si nous avons x produits, on lui demande de nous envoyer que 5
-			@RequestParam(name="size", defaultValue="5") int size) {
-		//Récupérer tout les produits dans un liste ou page ensuite il faut stocker la liste dans le modèle: donc il faut le déclarer dans l'entête de la méthode  List<Produit> produits=produitRepository.findAll();
+			@RequestParam(name="size", defaultValue="5") int size,
+			//Ajouter le paramètre motCle qui contient le mot recherché saisit par l'utilisateur
+			@RequestParam(name="motCle", defaultValue="")String motCle
+			) {
+		
+		//Récupérer tout les produits dans une liste ou page ensuite il faut stocker la liste dans le modèle: donc il faut le déclarer dans l'entête de la méthode  List<Produit> produits=produitRepository.findAll();
 		
 		//On utilise page et size que nous avons reçu comme paramètre
-		Page<Produit> pageProduits=produitRepository.findAll(PageRequest.of(page, size));
+		Page<Produit> pageProduits=produitRepository
+				.findByDesignationContains(motCle,PageRequest.of(page, size));
 		//On stocke dans le modèle un attribut =clé quis'appelle listProduits dont la valeur est la liste des produits
 		//Ensuite dans la vue il faut écrire le code thymeleaf pour faire une boucle sur les produits qui se trouve dans la liste des produits revenir vers la vue ==> products.html
 		model.addAttribute("pageProduits", pageProduits);
 		//Ajouter un attribut qui s'appelle pages et on créer un tableau de dimensions = nombre de pages ==> avoir les pages dans le modèle
 		model.addAttribute("currentPage", page);
 		model.addAttribute("size", size);
+		//Ajouter le mot clé dans le modèle
+		model.addAttribute("motCle", motCle);
 		model.addAttribute("pages", new int[pageProduits.getTotalPages()]);
 		return "products";
 	}
